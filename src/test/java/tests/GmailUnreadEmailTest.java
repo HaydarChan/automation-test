9be9410ffde4af81bda8a9c4a8bb19bc5932fc25
HaydarChan <atqiyahaydar15@gmail.com>
2025-04-27 23:24:44 +0700

    feat: working delete functionality

--- src/test/java/tests/GmailUnreadEmailTest.java
+++ src/test/java/tests/GmailUnreadEmailTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.*;
 import utils.DriverManager;
 
@@ -19,16 +20,15 @@ public class GmailUnreadEmailTest {
     @BeforeMethod
     public void setUp() {
         driver = DriverManager.getDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     @Test
-    public void testLogFiveUnreadEmailTitles() {
+    public void testLogAndDeleteFirstUnreadEmail() throws InterruptedException {
         driver.get("https://mail.google.com/");
 
         try {
             WebElement emailInput = driver.findElement(By.id("identifierId"));
-
             emailInput.sendKeys(System.getenv("GMAIL_EMAIL"));
             driver.findElement(By.id("identifierNext")).click();
 
@@ -42,14 +42,29 @@ public class GmailUnreadEmailTest {
         }
 
         List<WebElement> unreadEmails = driver.findElements(By.cssSelector("tr.zE"));
-        int maxEmails = Math.min(unreadEmails.size(), 5);
+        if (!unreadEmails.isEmpty()) {
+            WebElement firstUnread = unreadEmails.get(0);
+            String subject = firstUnread.findElement(By.cssSelector(".bog")).getText();
+            System.out.println("Subject of the unread email: " + subject);
 
-        if (maxEmails > 0) {
-            System.out.println("Subjects of up to 5 unread emails:");
-            for (int i = 0; i < maxEmails; i++) {
-                WebElement email = unreadEmails.get(i);
-                String subject = email.findElement(By.cssSelector(".bog")).getText();
-                System.out.println((i + 1) + ". " + subject);
+            WebElement firstUnreadCheckbox = firstUnread.findElement(By.xpath(".//div[@role='checkbox']"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", firstUnreadCheckbox);
+            Thread.sleep(500);
+
+            int initialUnreadCount = driver.findElements(By.cssSelector("tr.zE")).size();
+
+            WebElement deleteButtonInsideRow = firstUnread.findElement(By.cssSelector("li.bqX.bru div.KCRnif"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", deleteButtonInsideRow);
+
+            System.out.println("DELETED FROM ROW, waiting for update...");
+            Thread.sleep(2000); 
+
+            int afterUnreadCount = driver.findElements(By.cssSelector("tr.zE")).size();
+
+            if (afterUnreadCount < initialUnreadCount) {
+                System.out.println("Successfully deleted the unread email.");
+            } else {
+                throw new RuntimeException("Failed to delete the unread email after waiting!");
             }
         } else {
             System.out.println("No unread emails found.");
